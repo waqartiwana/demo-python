91050c6916e5714674677f1c79371af5880806e8
waqartiwana <waqartiwana3@gmail.com>
2025-11-14 12:35:40 +0500

    adding files in repo

---
+++ 01.py
@@ -0,0 +1,8 @@
+# ask user for their name and age and other details
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+address = input("Enter your address: ")
+phone = input("Enter your phone number: ")
+
+# print the details
+print(f"Name: {name}\nAge: {age}\nAddress: {address}\nPhone: {phone}")
